62dcef78353ef3ac984f338858392c784ef1014d
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2018-06-08 08:34:48 -0400

    Stopped using button toggle for editing the topic

--- src/topics/EditTopicForm.container.js
+++ src/topics/EditTopicForm.container.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
 import { editTopic } from './topics.actions';
-import TopicForm from './topicForm.component';
 import { connect } from 'react-redux';
+import { isEmpty } from 'ramda';
 
 class EditTopicForm extends Component {
+  state = {
+    name: ''
+  };
+
+  componentWillMount() {
+    const { name } = this.props;
+    if (!isEmpty(name))
+      this.setState({
+        ...this.state,
+        name
+      });
+  }
+
   update = ({ name }) => {
-    const { editTopic, topicId } = this.props;
+    const { editTopic, topicId, onSubmit } = this.props;
     editTopic({ topicId, name });
+    onSubmit();
+  };
+
+  submitHandler = e => {
+    e.preventDefault();
+
+    this.update({ name: this.state.name });
+  };
+
+  changeHandler = e => {
+    this.setState({
+      ...this.state,
+      name: e.target.value
+    });
   };
 
   render() {
-    const { name } = this.props;
     return (
       <div className="container text-right">
-        <TopicForm name={name} submitHandler={this.update} />
+        <form onSubmit={this.submitHandler.bind(this)}>
+          <div className="form-row">
+            <div className="col-auto form-group">
+              <input
+                className="form-control"
+                onChange={this.changeHandler.bind(this)}
+                value={this.state.name}
+                type="text"
+                id="topicName"
+              />
+            </div>
+            <div className="col-auto form-group">
+              <button type="submit" className="form-control btn btn-primary">
+                Save
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
--- src/topics/topic/Topic.container.js
+++ src/topics/topic/Topic.container.js
@@ -4,16 +4,44 @@ import EditTopicForm from '../EditTopicForm.container';
 import ButtonToggle from '../../button-toggle/ButtonToggle.container';
 
 class Topic extends Component {
+  state = {
+    edit: false
+  };
+
+  toggleEdit = () => {
+    this.setState({
+      ...this.state,
+      edit: !this.state.edit
+    });
+  };
+
+  onUpdate = () => {
+    this.setState({
+      ...this.state,
+      edit: false
+    });
+  };
+
   render() {
     const { topicName, topicId } = this.props;
+    const { edit } = this.state;
     return (
       <span className="container">
         <span className="row">
-          <span className="col-6">{topicName}</span>
-          <span className="col-6">
-            <ButtonToggle showIcon="fa fa-pencil" hideIcon="fa fa-times">
-              <EditTopicForm name={topicName} topicId={topicId} />
-            </ButtonToggle>
+          <span className="col-10">
+            {!edit && topicName}
+            {edit && (
+              <EditTopicForm
+                onSubmit={this.onUpdate}
+                name={topicName}
+                topicId={topicId}
+              />
+            )}
+          </span>
+          <span className="col">
+            <a onClick={this.toggleEdit.bind(this)}>
+              <span className={edit ? 'fa fa-times' : 'fa fa-pencil'} />
+            </a>
           </span>
         </span>
       </span>
