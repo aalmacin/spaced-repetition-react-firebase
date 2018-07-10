0903b95ff9e3d566f02567612a9a9424552a5910
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2018-07-10 19:13:07 -0400

    Added explanation on Next Study Calculator test

--- src/topics/study/nextStudyCalculator.test.js
+++ src/topics/study/nextStudyCalculator.test.js
@@ -1,65 +1,13 @@
 import * as R from 'ramda';
 
 describe('Next Study Calculator', () => {
-  const topics = [
-    {
-      name: 'Waves',
-      tags: ['Science', 'Physics'],
-      studies: [
-        {
-          date: '2018-07-05',
-          difficulty: 1,
-          minutes: 25
-        },
-        {
-          date: '2018-07-05',
-          difficulty: 1,
-          minutes: 25
-        },
-        {
-          date: '2018-07-06',
-          difficulty: 2,
-          minutes: 25
-        }
-      ]
-    },
-    {
-      name: 'Sounds',
-      tags: ['Science', 'Physics'],
-      studies: [
-        {
-          date: '2018-07-05',
-          difficulty: 1,
-          minutes: 25
-        },
-        {
-          date: '2018-07-05',
-          difficulty: 1,
-          minutes: 25
-        },
-        {
-          date: '2018-07-06',
-          difficulty: 2,
-          minutes: 25
-        },
-        {
-          date: '2018-07-08',
-          difficulty: 2,
-          minutes: 25
-        }
-      ]
-    },
-    {
-      name: 'Evolution',
-      tags: ['Science', 'Biology'],
-      studies: []
-    },
-    {
-      name: 'Limits',
-      tags: ['Calculus'],
-      studies: []
-    }
-  ];
-
-  // expect(actual).toBe([]);
+  // There are 5 boxes, Box 1 is the box where immediate action is required.
+  // Box 2 - 3 days
+  // Box 3 - 1 week
+  // Box 4 - 2 weeks
+  // Box 5 - 1 month
+  // Where the topic should be is the purpose of this calculator
+  // Every time a person makes a mistake, the item will be sent to Box 1. If the item is currently in Box 5,
+  // it will be set to Box 1. After the person studies the item again, the item will then be placed on Box 2.
+  expect(actual).toBe([]);
 });
